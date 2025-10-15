e4971de359758008832080fa2b6db2210c51da08
unknown <yeredmbe7@gmail.com>
2025-10-15 11:08:25 -0700

    adjusted the navbar bg color

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,9 +5,9 @@ const Footer = () => {
     <>
       <div className='w-full'>
         <div className='w-full py-8 max-sm:py-2 bg-[#222221] flex justify-around max-sm:flex-col max-sm:items-center max-sm:justify-center '>
-            <div className='flex items-center max-sm:my-3'><img src='./loaction.png' className='size-12 max-sm:size-8' /> <p className='text-xl max-sm:text-lg text-center text-white font-semibold ml-2'>Your Location</p> </div>
-            <div className='flex items-center max-sm:my-3'><img src='./telephone.png' className='size-12 max-sm:size-8' /><p className='text-xl max-sm:text-lg text-center text-white font-semibold  ml-2'>Your Phone</p> </div>
-            <div className='flex items-center max-sm:my-3'><img src='./mail.png' className='size-12 max-sm:size-8' /> <p className='text-xl max-sm:text-lg text-center text-white font-semibold  ml-2'>Your Email</p> </div>
+            <div className='flex items-center max-sm:my-2'><img src='./loaction.png' className='size-12 max-sm:size-8' /> <p className='text-xl max-sm:text-lg text-center text-white font-semibold ml-2'>Your Location</p> </div>
+            <div className='flex items-center max-sm:my-2'><img src='./telephone.png' className='size-12 max-sm:size-8' /><p className='text-xl max-sm:text-lg text-center text-white font-semibold  ml-2'>Your Phone</p> </div>
+            <div className='flex items-center max-sm:my-2'><img src='./mail.png' className='size-12 max-sm:size-8' /> <p className='text-xl max-sm:text-lg text-center text-white font-semibold  ml-2'>Your Email</p> </div>
         </div>
         <div className='py-8 max-sm:py-2 text-white bg-black flex justify-evenly items-center  max-sm:-flex max-sm:flex-col max-sm:items-center max-sm:justify-center '>
         {/* <hr className='border-1 border-red-600'/> */}
