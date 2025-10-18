2647ccc1c3d7c246c823a1b3159ed011be5adbea
unknown <yeredmbe7@gmail.com>
2025-10-18 02:16:46 -0700

    added aos on the contact page of the web site

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -43,7 +43,9 @@ const Contact = () => {
              className='w-full h-96 object-cover'
            />
            <div className='absolute left-1/2 top-1/2 text-center -translate-x-1/2 -translate-y-1/2 z-20 w-10/12'>
-             <h1 className='text-8xl text-white my-3 max-sm:text-5xl max-sm:mx-2 uppercase'>
+             <h1 className='text-8xl text-white my-3 max-sm:text-5xl max-sm:mx-2 uppercase' data-aos="slide-up"
+    data-aos-duration="1000"
+    data-aos-easing="ease-in-out">
             Nous Contacter
              </h1>
              <p className='text-3xl text-white max-sm:text-xl'>
@@ -57,22 +59,28 @@ const Contact = () => {
         <p className='text-white text-2xl my-4'>Demande de devis</p>
         <div className='w-full flex items-center justify-around max-sm:flex-col max-sm:items-center max-sm:justify-center'>
            <hr className=' border-[#9e8121]/40 w-1/5 max-sm:hidden'/> 
-           <p className='text-5xl text-center max-sm:px-3 max-sm:text-center max-sm:text-3xl text-[#271a02]'>Réservez votre Photobooth</p>
+           <p className='text-5xl text-center max-sm:px-3 max-sm:text-center max-sm:text-3xl text-[#271a02]' data-aos="slide-up"
+          data-aos-delay="50"
+    data-aos-duration="1000"
+    data-aos-easing="ease-in-out">Réservez votre Photobooth</p>
            <hr className=' border-[#9e8121]/40 w-1/5 max-sm:hidden'/> 
         </div>
-        <p className='w-2/5 text-center my-4 max-sm:w-full max-sm:px-4 font-Sans-Font max-sm:text-lg text-[#271a02]'>Simplifiez votre réservation de photobooth grâce à notre formulaire convivial. Saisissez vos coordonnées et les informations relatives à l'événement pour obtenir une réponse rapide de notre part.</p>
+        <p className='w-2/5 text-center my-4 max-sm:w-full max-sm:px-4 font-Sans-Font max-sm:text-lg text-[#271a02]' data-aos="slide-up"
+          data-aos-delay="100"
+    data-aos-duration="1000"
+    data-aos-easing="ease-in-out">Simplifiez votre réservation de photobooth grâce à notre formulaire convivial. Saisissez vos coordonnées et les informations relatives à l'événement pour obtenir une réponse rapide de notre part.</p>
          
       </div>
       </div>
-
-
       <PhotoboothForm />
-
          <div className='w-full flex flex-col items-center justify-center my-8 py-8 font-GaramondAntiqua'>
         <p className='text-[#9e8121]/50 text-3xl my-3 '>Les étapes clés</p>
         <div className='w-10/12 max-sm:w-full flex items-center justify-around max-sm:flex-col max-sm:items-center max-sm:justify-center'>
            <hr className=' border-[#9e8121]/40 w-1/5 max-sm:hidden'/> 
-           <p className='text-6xl mx-8 max-sm:mx-3 text-center max-sm:px-3 max-sm:text-center max-sm:text-4xl text-black/75'>Processus de location de photobooth</p>
+           <p className='text-6xl mx-8 max-sm:mx-3 text-center max-sm:px-3 max-sm:text-center max-sm:text-4xl text-black/75' data-aos="slide-up"
+          data-aos-delay="50"
+    data-aos-duration="1000"
+    data-aos-easing="ease-in-out">Processus de location de photobooth</p>
            <hr className=' border-[#9e8121]/40 w-1/5 max-sm:hidden'/> 
         </div>
       </div>
@@ -80,19 +88,27 @@ const Contact = () => {
 <div className='w-full flex flex-col items-center justify-center my-2 py-8 font-GaramondAntiqua'>
       <div className='w-10/12 max-sm:px-4 max-sm:w-full py-7 max-sm:py-2 text-center flex items-center justify-around max-sm:flex-col max-sm:items-center max-sm:justify-between'>
         <div>
-            <h1 className='text-9xl text-[#9e8121]/70 font-AdobeHebrew-italic'>01</h1>
+            <h1 className='text-9xl text-[#9e8121]/70 font-AdobeHebrew-italic' data-aos="slide-left"
+    data-aos-duration="1000"
+    data-aos-easing="ease-in-out">01</h1>
             <p className='text-3xl my-2'>Demande de devis</p>
             <p className='font-Sans-Font text-black/65'>Remplissez notre formulaire avec les détails de votre Photobooth et de votre événement pour un devis personnalisé.</p>
         </div>
 
        <div className='mx-4 max-sm:mx-0 max-sm:my-6 '>
-            <h1 className='text-9xl text-[#9e8121]/70 font-AdobeHebrew-italic'>02</h1>
+            <h1 className='text-9xl text-[#9e8121]/70 font-AdobeHebrew-italic' data-aos="slide-left"
+            data-aos-delay="100"
+    data-aos-duration="1000"
+    data-aos-easing="ease-in-out">02</h1>
             <p className='text-3xl my-2'>Réservation de service</p>
             <p className='font-Sans-Font text-black/65'>Nous vous contacterons le plus rapidement possible pour discuter de vos besoins, fixer la date de votre événement et commencer les préparatifs.</p>
         </div>
 
         <div>
-            <h1 className='text-9xl text-[#9e8121]/70 font-AdobeHebrew-italic'>03</h1>
+            <h1 className='text-9xl text-[#9e8121]/70 font-AdobeHebrew-italic' data-aos="slide-left"
+            data-aos-delay="150"
+    data-aos-duration="1000"
+    data-aos-easing="ease-in-out">03</h1>
             <p className='text-3xl my-2'>Jour de l'événement</p>
             <p className='font-Sans-Font text-black/65'>Notre équipe livre, installe, assure une expérience sans faille et vous envoie un dossier cloud après l’événement.</p>
         </div>
@@ -107,7 +123,11 @@ const Contact = () => {
         className='w-1/2 max-sm:w-full object-cover'
       />
       <div className='w-1/2 relative ml-16 p-5 max-sm:ml-0 max-sm:w-full max-sm:py-4 max-sm:text-center font-Sans-Font text-[#271a02] max-sm:bg-[#9e8121]/40'>
-        <h1 className='text-5xl my-2 font-GaramondAntiqua max-sm:text-3xl'>
+        <h1 className='text-5xl my-2 font-GaramondAntiqua max-sm:text-3xl'
+        data-aos="slide-right"
+            data-aos-delay="50"
+    data-aos-duration="1000"
+    data-aos-easing="ease-in-out">
           Voici ce que disent de nous
         </h1>
         <img src="./arrow.png" alt="" className='size-32 absolute top-1 left-0 max-sm:hidden -z-10 opacity-55' />
