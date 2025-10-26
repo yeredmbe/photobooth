0f2fd6565e3183f0c8ce5c5507b9b0ae4e30bf88
unknown <yeredmbe7@gmail.com>
2025-10-26 14:43:41 -0700

    continue changing the images

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -51,7 +51,7 @@ const Body = () => {
     data-aos-duration="1000"
     data-aos-easing="ease-in-out">Vous voulez que votre prochain événement se démarque ?</p>
         <div className=' w-1/2  max-sm:w-11/12 max-sm:py-4' >
-            <p className='text-[#9e8121]/50 text-3xl mb-1 max-sm:text-2xl max-sm:text-center max-sm:my-6 uppercase'
+            <p className='text-[#e25d1f]/50 text-3xl mb-1 max-sm:text-2xl max-sm:text-center max-sm:my-6 uppercase'
              data-aos="slide-up"
     data-aos-duration="1000"
     data-aos-easing="ease-in-out">Si l’originalité vous préoccupe pour vos souvenirs inoubliables, notre Photobooth répond à ce besoin !</p>
@@ -67,7 +67,7 @@ const Body = () => {
     data-aos-delay="50"
     data-aos-duration="1000"
     data-aos-easing="ease-in-out" />
-            <div className='w-1/2 py-14 max-sm:py-3 bg-[#9e8121]/30 grid grid-cols-2 gap-4 p-4 max-sm:w-full max-sm:grid-cols-1 max-sm:my-4'>
+            <div className='w-1/2 py-14 max-sm:py-3 bg-[#e25d1f]/30 grid grid-cols-2 gap-4 p-4 max-sm:w-full max-sm:grid-cols-1 max-sm:my-4'>
                 <div className='justify-center items-center flex flex-col ' >
                     <img src='./emoji.png' alt='smiley'  className='size-16'/>
                     <p className='text-4xl my-1'>Say Cheese !</p>
@@ -95,17 +95,17 @@ Avec Elikiabooth, vos plus beaux souvenirs se partagent en un clic !</p>
    
 
       <div className='w-full flex flex-col items-center justify-center my-8 py-8'>
-        <p className='text-[#9e8121]/50 text-xl max-sm:hidden my-2'>NOS SÉLECTIONS</p>
+        <p className='text-[#e25d1f]/50 text-xl max-sm:hidden my-2'>NOS SÉLECTIONS</p>
         <p className='max-sm:text-6xl text-6xl max-sm:my-2'  data-aos="slide-up"
     data-aos-duration="1000"
     data-aos-easing="ease-in-out">Nos photobooths</p>
         <div className='w-full flex items-center justify-around max-sm:flex-col max-sm:items-center max-sm:justify-center'>
-           <hr className=' border-[#9e8121]/40 w-1/5 max-sm:hidden'/> 
+           <hr className=' border-[#e25d1f]/40 w-1/5 max-sm:hidden'/> 
            <p className='text-3xl max-sm:px-3 max-sm:text-center max-sm:text-2xl text-black/75' data-aos="slide-up"
     data-aos-delay="30"
     data-aos-duration="1000"
     data-aos-easing="ease-in-out">Consultez nos différentes options de location.</p>
-           <hr className=' border-[#9e8121]/40 w-1/5 max-sm:hidden'/> 
+           <hr className=' border-[#e25d1f]/40 w-1/5 max-sm:hidden'/> 
         </div>
          <Link to={"/photobooth"} className="mt-6 mx-3 cursor-pointer px-6 py-3 bg-[#000] border border-[#000]  text-white text-xl font-medium hover:text-black  hover:bg-white transition duration-500 hover:border-black ease-in-out">EN APPRENDRE D'AVANTAGE</Link>
       </div>
@@ -122,12 +122,12 @@ Avec Elikiabooth, vos plus beaux souvenirs se partagent en un clic !</p>
     data-aos-easing="ease-in-out"
           >
             <img src={image.img} alt="" className='w-full hover:translate-y-[-10px] transition-transform duration-500 ease-in-out' />
-            <div className='absolute bottom-0 left-0 right-0 bg-[#ad933b]  text-[#271a02] text-center p-4 text-4xl max-sm:text-2xl'>{image.title}</div>
+            <div className='absolute bottom-0 left-0 right-0 bg-[#e25d1f]  text-[#271a02] text-center p-4 text-4xl max-sm:text-2xl'>{image.title}</div>
 </div>))
          }
       </div>
 
-      <div className='bg-[#a58e42]/80 w-full py-8 max-sm:my-1 my-8 grid grid-cols-4 max-sm:grid-cols-1 gap-4 max-sm:gap-2 items-center justify-center'>
+      <div className='bg-[#e25d1f]/80 w-full py-8 max-sm:my-1 my-8 grid grid-cols-4 max-sm:grid-cols-1 gap-4 max-sm:gap-2 items-center justify-center'>
         {
           data.length > 0 && data.map((item, index) => (
             <div 
@@ -143,7 +143,7 @@ Avec Elikiabooth, vos plus beaux souvenirs se partagent en un clic !</p>
       </div>
 
       <div className='w-full flex flex-col justify-center items-center max-sm:my-0 my-12 max-sm:hidden'>
-        <h1 className='text-center text-2xl text-[#9e8121]/50 my-3 uppercase'>Photobooth personnalisé</h1>
+        <h1 className='text-center text-2xl text-[#e25d1f]/50 my-3 uppercase'>Photobooth personnalisé</h1>
         <h2 className='text-center text-5xl w-10/12 font-AdobeHebrew' 
         data-aos="slide-up"
     data-aos-duration="1000"
@@ -158,7 +158,7 @@ data-aos="slide-up"
           data1.length > 0 && data1.map((item, index) => (
             <div 
             key={index}
-            className='w-full flex bg-[#ad933b]/80 mx-3 flex-col items-center hover:scale-[1.1] hover:translate-y-[-15px] transition-transform duration-500 ease-in-out justify-center text-center py-8 px-4 max-sm:py-4 max-sm:px-2 text-black max-sm:my-4'>
+            className='w-full flex bg-[#e25d1f]/80 mx-3 flex-col items-center hover:scale-[1.1] hover:translate-y-[-15px] transition-transform duration-500 ease-in-out justify-center text-center py-8 px-4 max-sm:py-4 max-sm:px-2 text-black max-sm:my-4'>
               <div className='size-24  rounded-full p-3  flex items-center  justify-center mx-auto'>
                  <img src={item.icon} className='size-16' />  
               </div>
@@ -169,7 +169,7 @@ data-aos="slide-up"
         </div>
       </div>
 
-      <div className='w-full flex items-center justify-center max-sm:flex-col max-sm:items-center max-sm:justify-center max-sm:px-0 max-sm:my-0 my-12 max-sm:py-0 py-12 bg-[#9e8121]/40 px-24'>
+      <div className='w-full flex items-center justify-center max-sm:flex-col max-sm:items-center max-sm:justify-center max-sm:px-0 max-sm:my-0 my-12 max-sm:py-0 py-12 bg-[#e25d1f]/40 px-24'>
       <img src="photo_3_2025-10-18_13-20-11.jpg" alt="" className='w-1/2 max-sm:w-full' />
       <div className='w-1/2 ml-16 max-sm:ml-0 max-sm:w-full max-sm:py-4 max-sm:text-center font-Sans-Font text-[#271a02]'>
         <h1 className='text-4xl my-5 font-GaramondAntiqua'
@@ -196,15 +196,15 @@ data-aos="slide-up"
       </div>
       </div>
 
-        <div className='w-full  flex flex-col items-center justify-center max-sm:my-0 mt-2 bg-[#9e8121]/40 py-8'>
+        <div className='w-full  flex flex-col items-center justify-center max-sm:my-0 mt-2 bg-[#e25d1f]/40 py-8'>
         <div className='w-10/12 max-sm:w-full flex flex-col items-center justify-center '>
         <p className='text-white text-2xl my-4'>Demande de réservation</p>
         <div className='w-full flex items-center justify-around max-sm:flex-col max-sm:items-center max-sm:justify-center'>
-           <hr className=' border-[#9e8121]/40 w-1/5 max-sm:hidden'/> 
+           <hr className=' border-[#e25d1f]/40 w-1/5 max-sm:hidden'/> 
            <p className='text-5xl text-center max-sm:px-3 max-sm:text-center max-sm:text-3xl text-[#271a02]' data-aos="slide-up"
     data-aos-duration="1000"
     data-aos-easing="ease-in-out">Souriez, immortalisez, revivez chaque instant.</p>
-           <hr className=' border-[#9e8121]/40 w-1/5 max-sm:hidden'/> 
+           <hr className=' border-[#e25d1f]/40 w-1/5 max-sm:hidden'/> 
         </div>
         <p className='w-2/5 text-center my-4 max-sm:w-full max-sm:px-4 font-Sans-Font max-sm:text-lg text-[#271a02]' data-aos="slide-up"
     data-aos-delay="50"
